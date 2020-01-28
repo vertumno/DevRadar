1715a4ad38e14ff9a7a0175fe1e59c29a44b165a
Lucas Dantas <lucas_gomesdantas@hotmail.com>
2020-01-28 07:29:17 -0300

    Bootstraping the mobile part of the application, using Expo.

---
+++ mobile/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Lucas Dantas</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  title: {
+    color: '#fff',
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+});
