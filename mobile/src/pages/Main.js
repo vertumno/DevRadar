e3711e5bdd0daf26e040f9cf6b42463c99087fd9
Lucas Dantas <lucas_gomesdantas@hotmail.com>
2020-01-28 20:14:54 -0300

    Creating a form to search devs by techs. Implementing styles for it.

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'; 
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
@@ -30,20 +31,34 @@ function Main({ navigation }) {
     }
 
     return (
-        <MapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={{ latitude: -5.8127497, longitude: -35.2258358}}>
-                <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/17501172?s=460&v=4'}} />
-                <Callout onPress={() => {
-                    navigation.navigate('Profile', { github_username: 'vertumno' });
-                }}>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Lucas Dantas</Text>
-                        <Text style={styles.devBio}>aiming to make a difference.</Text>
-                        <Text style={styles.devTechs}>ReactJS, React Native, NodeJS.</Text>
-                    </View>
-                </Callout>
-            </Marker>    
-        </MapView>    
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+                <Marker coordinate={{ latitude: -5.8127497, longitude: -35.2258358}}>
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/17501172?s=460&v=4'}} />
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', { github_username: 'vertumno' });
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Lucas Dantas</Text>
+                            <Text style={styles.devBio}>aiming to make a difference.</Text>
+                            <Text style={styles.devTechs}>ReactJS, React Native, NodeJS.</Text>
+                        </View>
+                    </Callout>
+                </Marker>    
+            </MapView>
+            <View style={styles.searchForm}>
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder="Search developer by techs..."
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+                <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                    <MaterialIcons name="my-location" size={20} color="#fff" />
+                </TouchableOpacity>
+            </View>
+        </>
     )        
 
 }
@@ -78,6 +93,42 @@ const styles = StyleSheet.create({
     devTechs: {
         marginTop: 5
     },
+
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#fff',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 2
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8E4DFF',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15
+    }
 });
 
 export default Main;
