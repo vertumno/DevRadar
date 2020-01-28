c1fe5767d3546493a8fb61d3786ac2bb6138ecfe
Lucas Dantas <lucas_gomesdantas@hotmail.com>
2020-01-28 07:50:38 -0300

    Creating first routes. Installing components required for navigation.

--- mobile/App.js
+++ mobile/App.js
@@ -1,25 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Lucas Dantas</Text>
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    color: '#fff',
-    fontSize: 32,
-    fontWeight: 'bold',
-  },
-});
