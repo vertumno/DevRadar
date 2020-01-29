88ccd46ed059367a83be0aa5626687ba1995e157
Lucas Dantas <lucas_gomesdantas@hotmail.com>
2020-01-28 21:43:22 -0300

    Trying to communicate with backend API. Still no success.

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -4,8 +4,12 @@ import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'; 
 import { MaterialIcons } from '@expo/vector-icons';
 
+import api from '../services/api';
+
 function Main({ navigation }) {
+    const [devs, setDevs] = useState([]);
     const [currentRegion, setCurrentRegion] = useState(null);
+    const [techs, setTechs] = useState('');
     
     useEffect(() => {
         async function loadInitialPosition(){
@@ -26,25 +30,50 @@ function Main({ navigation }) {
         loadInitialPosition();
     }, []);
 
+    async function loadDevs() {
+        //const { latitude, longitude } = currentRegion;
+        const latitude = -5.8127571;
+        const longitude = -35.2258257;
+        const response = api.get('/search', {
+            params: {
+                latitude,
+                longitude,
+                techs: 'ReactJS'
+            }
+        });
+        setDevs(response.data.devs);
+    }
+
+    function handleRegionChanged(region) {
+        setCurrentRegion(region);
+    }
+
     if (!currentRegion) {
         return null;
     }
 
     return (
         <>
-            <MapView initialRegion={currentRegion} style={styles.map}>
-                <Marker coordinate={{ latitude: -5.8127497, longitude: -35.2258358}}>
-                    <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/17501172?s=460&v=4'}} />
-                    <Callout onPress={() => {
-                        navigation.navigate('Profile', { github_username: 'vertumno' });
-                    }}>
-                        <View style={styles.callout}>
-                            <Text style={styles.devName}>Lucas Dantas</Text>
-                            <Text style={styles.devBio}>aiming to make a difference.</Text>
-                            <Text style={styles.devTechs}>ReactJS, React Native, NodeJS.</Text>
-                        </View>
-                    </Callout>
-                </Marker>    
+            <MapView onRegionChangeComplete={handleRegionChanged} initialRegion={currentRegion} style={styles.map}>
+                {devs.map(dev => (
+                    <Marker
+                        key={dev._id} 
+                        coordinate={{
+                            latitude: dev.location.coordinates[1],
+                            longitude: dev.location.coordinates[0]
+                        }}                    >
+                        <Image style={styles.avatar} source={{ uri: dev.avatar_url }} />
+                        <Callout onPress={() => {
+                            navigation.navigate('Profile', { github_username: dev.github_username });
+                        }}>
+                            <View style={styles.callout}>
+                                <Text style={styles.devName}>{dev.name}</Text>
+                                <Text style={styles.devBio}>{dev.bio}</Text>
+                                <Text style={styles.devTechs}>{dev.techs.join(', ')}</Text>
+                            </View>
+                        </Callout>
+                    </Marker>
+                ))}      
             </MapView>
             <View style={styles.searchForm}>
                 <TextInput
@@ -53,14 +82,15 @@ function Main({ navigation }) {
                     placeholderTextColor="#999"
                     autoCapitalize="words"
                     autoCorrect={false}
+                    value={techs}
+                    onChangeText={setTechs}
                 />
-                <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                <TouchableOpacity onPress={loadDevs} style={styles.loadButton}>
                     <MaterialIcons name="my-location" size={20} color="#fff" />
                 </TouchableOpacity>
             </View>
         </>
     )        
-
 }
 
 const styles = StyleSheet.create({
